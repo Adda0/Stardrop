175f04e8da870694ae615fdf2d023e25c6f9aca9
David Chocholatý <chocholaty.david@protonmail.com>
2023-11-26 19:58:33 +0100

    Filter on Group rows as well

--- Stardrop/ViewModels/MainWindowViewModel.cs
+++ Stardrop/ViewModels/MainWindowViewModel.cs
@@ -582,7 +582,11 @@ namespace Stardrop.ViewModels
             }
             if (!String.IsNullOrEmpty(_filterText) && !String.IsNullOrEmpty(_columnFilter))
             {
-                if (_columnFilter == Program.translation.Get("ui.main_window.combobox.mod_name") && !mod.Name.Contains(_filterText, StringComparison.OrdinalIgnoreCase))
+                if (_columnFilter == Program.translation.Get("ui.main_window.combobox.mod_name")
+                    // Try filtering on mod path name (group name).
+                    && !mod.Path.Contains(_filterText, StringComparison.OrdinalIgnoreCase)
+                    // Try filtering on Mod name.
+                    && !mod.Name.Contains(_filterText, StringComparison.OrdinalIgnoreCase))
                 {
                     return false;
                 }
