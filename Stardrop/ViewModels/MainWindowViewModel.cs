8eb366354c87b1de80a07f4b40a84690ebbd8aca
David Chocholatý <chocholaty.david@protonmail.com>
2023-11-27 19:27:27 +0100

    Don't group by author

--- Stardrop/ViewModels/MainWindowViewModel.cs
+++ Stardrop/ViewModels/MainWindowViewModel.cs
@@ -79,15 +79,14 @@ namespace Stardrop.ViewModels
             Version = $"v{version}";
             SmapiVersion = Program.settings.GameDetails?.SmapiVersion;
 
-            // Create data view
+            // Create data view.
             var dataGridSortDescription = DataGridSortDescription.FromPath(nameof(Mod.Name), ListSortDirection.Ascending);
             var dataGridGroupDescriptionPath = new DataGridPathGroupDescription(nameof(Mod.Path));
             var dataGridGroupDescriptionAuthor = new DataGridPathGroupDescription(nameof(Mod.Author));
-            
 
             DataView = new DataGridCollectionView(Mods, false, false);
             DataView.SortDescriptions.Add(dataGridSortDescription);
-            DataView.GroupDescriptions.Add(dataGridGroupDescriptionAuthor);
+            // DataView.GroupDescriptions.Add(dataGridGroupDescriptionAuthor);
             DataView.GroupDescriptions.Add(dataGridGroupDescriptionPath);
             UpdateFilter();
 
@@ -563,6 +562,7 @@ namespace Stardrop.ViewModels
         private bool ModFilter(object item)
         {
             var mod = item as Mod;
+            if (mod == null) { return false; }
 
             if (mod.IsHidden)
             {
@@ -603,6 +603,6 @@ namespace Stardrop.ViewModels
             }
 
             return true;
-        }
+        } // ModFilter().
     }
 }
