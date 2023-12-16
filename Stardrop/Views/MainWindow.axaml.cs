17444b5550e4dd34ef2c8210722255ae2afabac4
David Chocholatý <chocholaty.david@protonmail.com>
2023-12-16 20:31:40 +0100

    Allow manually installing multiple mods

--- Stardrop/Views/MainWindow.axaml.cs
+++ Stardrop/Views/MainWindow.axaml.cs
@@ -1059,7 +1059,7 @@ namespace Stardrop.Views
 
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Filters.Add(new FileDialogFilter() { Name = "Mod Archive (*.zip, *.7z, *.rar)", Extensions = { "zip", "7z", "rar" } });
-            dialog.AllowMultiple = false;
+            dialog.AllowMultiple = true;
 
             var addedMods = await AddMods(await dialog.ShowAsync(this));
 
