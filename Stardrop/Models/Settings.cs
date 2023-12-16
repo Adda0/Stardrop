ab780d4f6a81c35e191021af4d311388306d9685
David Chocholatý <chocholaty.david@protonmail.com>
2023-12-16 10:15:02 +0100

    Declare possible nullable values

--- Stardrop/Models/Settings.cs
+++ Stardrop/Models/Settings.cs
@@ -7,25 +7,25 @@ namespace Stardrop.Models
     public class Settings
     {
         public string Theme { get; set; } = "Stardrop";
-        public string Language { get; set; }
-        public string Version { get; set; }
-        public string LastSelectedProfileName { get; set; }
-        public string SMAPIFolderPath { get; set; }
-        public string ModFolderPath { get; set; }
-        public string ModInstallPath { get; set; }
-        public string FolderForModsToAddPath { get; set; }
+        public string? Language { get; set; }
+        public string? Version { get; set; }
+        public string? LastSelectedProfileName { get; set; }
+        public string? SMAPIFolderPath { get; set; }
+        public string? ModFolderPath { get; set; }
+        public string? ModInstallPath { get; set; }
+        public string? FolderForModsToAddPath { get; set; }
         public bool IgnoreHiddenFolders { get; set; } = true;
         public bool EnableProfileSpecificModConfigs { get; set; }
         public bool ShouldWriteToModConfigs { get; set; }
-        public bool EnableModsOnAdd { get; set; }
+        public bool EnableModsOnAdd { get; set; } = false;
         /// <summary>
         /// Whether to always ask before deleting a previous version of a mod when updating the mod.
         /// </summary>
         public bool AlwaysAskToDelete { get; set; } = true;
         public NexusServers PreferredNexusServer { get; set; } = NexusServers.NexusCDN;
         public bool IsAskingBeforeAcceptingNXM { get; set; } = true;
-        public GameDetails GameDetails { get; set; }
-        public NexusUser NexusDetails { get; set; }
+        public GameDetails? GameDetails { get; set; }
+        public NexusUser? NexusDetails { get; set; }
 
         public Settings ShallowCopy()
         {
